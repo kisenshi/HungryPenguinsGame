d1e83a1aa1549ce1e2133a3aa8445761ebb9999d
Cristina Guerrero Romero <cguer001@cgs-macd-16955.campus.goldsmiths.ac.uk>
2016-11-02 12:33:10 +0000

    Basic Scene

---
+++ Assets/GroundDetect.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundDetect : MonoBehaviour {
+
+     PenguinController character;
+
+    void Start()
+    {
+        character = transform.parent.gameObject.GetComponent<PenguinController>();
+    }
+
+    private bool isGround(Collider2D c)
+    {
+        return true;
+    }
+
+    void OnTriggerEnter2D()
+    {
+
+        if (character)
+        {
+            character.groundHitYN = true;
+        }
+    }
+
+    void OnTriggerStay2D()
+    {
+
+        if (character)
+        {
+            character.groundHitYN = true;
+        }
+    }
+
+    void OnTriggerExit2D()
+    {
+
+        if (character)
+        {
+            character.groundHitYN = false;
+        }
+    }
+}
+
