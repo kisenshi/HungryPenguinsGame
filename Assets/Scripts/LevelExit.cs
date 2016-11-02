7cbc852d65b75913ad6fa85b843f352ef24ffef5
Cristina Guerrero Romero <cguer001@cgs-macd-16955.campus.goldsmiths.ac.uk>
2016-11-02 15:23:37 +0000

    End level works

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelExit : MonoBehaviour {
+
+	public GameObject display;
+	private FishDisplay fd;
+
+	void Start(){
+	
+		fd = display.GetComponent<FishDisplay> ();
+	
+	}
+
+	private bool levelComplete(){
+		return fd.collected >= fd.fishes;
+	}
+
+	void OnTriggerEnter2D(Collider2D c){
+		if (levelComplete() && c.gameObject.name == "Penguin") {
+			Debug.Log ("Works!");
+		}
+	}
+}
