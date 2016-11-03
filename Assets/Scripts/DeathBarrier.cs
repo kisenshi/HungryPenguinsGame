efa89db84279b26042b32243d5214462f2f92f31
Cristina Guerrero Romero <cguer001@cgs-macd-13207.campus.goldsmiths.ac.uk>
2016-11-03 10:53:30 +0000

    RestartBtn created and working

--- Assets/Scripts/DeathBarrier.cs
+++ Assets/Scripts/DeathBarrier.cs
@@ -1,14 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
 
 public class DeathBarrier : MonoBehaviour {
 
-	private void restartCurrentLevel()
-	{
-		int scene = SceneManager.GetActiveScene().buildIndex;
-		SceneManager.LoadScene(scene, LoadSceneMode.Single);
-	}
+
 
 
 	void OnTriggerEnter2D(Collider2D c)
@@ -16,7 +11,7 @@ public class DeathBarrier : MonoBehaviour {
 
 		if (c.gameObject.name == "Penguin")
 		{
-			restartCurrentLevel();
+			GameManager.restartLevel();
 		}
 	}
 
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public static class GameManager {
+
+	public static void restartLevel(){
+
+		int scene = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(scene, LoadSceneMode.Single);
+}
+
+}
