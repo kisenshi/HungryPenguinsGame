cc3793353f77ecdb296ef94733b7db086f107daa
Rory Davidson <rdavi019@cgs-macd-13208.campus.goldsmiths.ac.uk>
2016-11-04 15:20:34 +0000

    Fliddling with icebox

--- Assets/BoxSlide.cs
+++ Assets/BoxSlide.cs
@@ -6,22 +6,44 @@ public class BoxSlide : MonoBehaviour {
 	public float slidespeed;
 	public float slideaccel;
 
-	private int slidedir;
+	private bool stop;
+
+//	private bool sliding;
+//	private int i=0;
+
 
 	void LateUpdate(){
+		
 		Rigidbody2D vel = gameObject.GetComponent<Rigidbody2D>();
 		float y = vel.velocity.y;
 		float x = vel.velocity.x;
 
-		if (x != 0 && x < slidespeed) {
-			vel.AddForce (x > 0 ? transform.right * slideaccel : -transform.right * slideaccel, ForceMode2D.Force);
-			//slidedir = x > 0 ? 1 : 0;
+
+		/**if (sliding&&x==0) {
+			i++;
+			sliding = i > 10 ? false : sliding;
+			i = i > 10 ? 0 : i;
+		}*/
+
+		if (stop) {
+			vel.velocity = new Vector2 (0, 0);
 		}
+
+		else if (x != 0 && x < slidespeed) {
+			vel.AddForce (x > 0 ? transform.right * slideaccel : -transform.right * slideaccel, ForceMode2D.Force);
+		}
+
+
 	}
 
 	void OnCollisionEnter2D(Collision2D c){
-		if (c.gameObject.name == "WoodBox")
-			Debug.Log ("crash");
-			gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+
+		Rigidbody2D vel = gameObject.GetComponent<Rigidbody2D>();
+
+		if ((c.gameObject.name == "WoodBox" || c.gameObject.name == "IceBox")) {
+			gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, vel.velocity.y);
+			c.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, c.gameObject.GetComponent<Rigidbody2D> ().velocity.y);
+			stop = true;
+		}
 	}
 }
