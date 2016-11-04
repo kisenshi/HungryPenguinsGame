b7ca97bc35b9ed2eab582ac374947bc01f9cbe2a
Rory Davidson <rdavi019@cgs-macd-13208.campus.goldsmiths.ac.uk>
2016-11-04 16:37:41 +0000

    Fixed the ice block, mostly

---
+++ Assets/BoxPush.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxPush : MonoBehaviour {
+
+	private bool pushing;
+	private int pushtimer;
+
+	void Start(){
+		pushing = false;
+		pushtimer = 0;
+	}
+
+	void OnCollisionEnter2D(Collision2D c){
+
+	}
+}
--- Assets/BoxSlide.cs
+++ Assets/BoxSlide.cs
@@ -8,8 +8,8 @@ public class BoxSlide : MonoBehaviour {
 
 	private bool stop;
 
-//	private bool sliding;
-//	private int i=0;
+
+
 
 
 	void LateUpdate(){
@@ -19,17 +19,9 @@ public class BoxSlide : MonoBehaviour {
 		float x = vel.velocity.x;
 
 
-		/**if (sliding&&x==0) {
-			i++;
-			sliding = i > 10 ? false : sliding;
-			i = i > 10 ? 0 : i;
-		}*/
 
-		if (stop) {
-			vel.velocity = new Vector2 (0, 0);
-		}
 
-		else if (x != 0 && x < slidespeed) {
+		if (!stop && x != 0 && x < slidespeed) {
 			vel.AddForce (x > 0 ? transform.right * slideaccel : -transform.right * slideaccel, ForceMode2D.Force);
 		}
 
@@ -43,7 +35,12 @@ public class BoxSlide : MonoBehaviour {
 		if ((c.gameObject.name == "WoodBox" || c.gameObject.name == "IceBox")) {
 			gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, vel.velocity.y);
 			c.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, c.gameObject.GetComponent<Rigidbody2D> ().velocity.y);
-			stop = true;
+
+			Vector2 dir = c.contacts [0].point - (Vector2)transform.position;
+			dir = -dir.normalized;
+
+			transform.position = new Vector2 (transform.position.x + dir.x * 0.01f, transform.position.y);
+
 		}
 	}
 }
