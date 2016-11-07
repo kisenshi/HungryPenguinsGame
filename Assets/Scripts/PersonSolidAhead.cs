4a9f968352d1832c38b459ae7fc9e67436526182
Rory Davidson <rd553@york.ac.uk>
2016-11-07 17:49:06 +0000

    Person-walking-into-things logic added

---
+++ Assets/Scripts/PersonSolidAhead.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonSolidAhead : MonoBehaviour {
+
+    public Grounded person;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (person.gameObject.layer == 11) person = transform.parent.gameObject.GetComponent<PersonMovement>();
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+
+        if (person && (c.gameObject.layer == 9 || c.gameObject.layer == 10))
+        {
+            person.groundHitYN = false;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D c)
+    {
+
+        if (person && (c.gameObject.layer == 9 || c.gameObject.layer == 10))
+        {
+            person.groundHitYN = false;
+        }
+    }
+}
