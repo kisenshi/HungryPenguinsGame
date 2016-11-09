f98bd9c22e5f2f0cd5b626daae59298472d499d2
Rory Davidson <rdavi019@cgs-macd-16904.campus.goldsmiths.ac.uk>
2016-11-09 13:05:29 +0000

    Misc physics fixes

---
+++ Assets/Scripts/WoodBox.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WoodBox : MonoBehaviour {
+
+
+
+	public void Freeze(){
+
+			gameObject.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+
+	}
+
+	public void unFreeze(){
+		gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+	}
+
+
+
+	}
+
+
